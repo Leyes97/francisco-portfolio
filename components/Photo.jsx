3f57ad13d440717d27faf50596f23da1b9d0dcbe
francisco <francisco.leyescampoy@gmail.com>
2024-08-16 14:13:35 -0300

    added

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,29 +1,10 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import Image from 'next/image';
 
 const Photo = () => {
   return (
-    <div className="relative w-[300px] h-[300px] xl:w-[500px] xl:h-[500px] overflow-hidden flex items-center justify-center">
-      {/* Image */}
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{
-          opacity: 1,
-          transition: { delay: 2, duration: 0.4, ease: 'easeInOut' },
-        }}
-        className="absolute w-full h-full rounded-full overflow-hidden mix-blend-difference">
-        <Image
-          src="/assets/francisco.cv.png"
-          priority
-          quality={100}
-          fill
-          alt=""
-          className="object-cover"
-        />
-      </motion.div>
-
+    <div className="relative w-[300px] h-[300px] xl:w-[500px] xl:h-[500px] overflow-hidden flex items-center justify-center bg-primary">
       {/* Circle */}
       <motion.svg
         className="absolute top-0 left-0 w-full h-full"
@@ -31,22 +12,39 @@ const Photo = () => {
         viewBox="0 0 506 506"
         xmlns="http://www.w3.org/2000/svg">
         <motion.circle
-          cx="252"
-          cy="252"
-          r="252"
+          cx="253"
+          cy="253"
+          r="200"
           stroke="#ffe033"
-          strokeWidth="6"
+          strokeWidth="12"
           strokeLinecap="round"
           strokeLinejoin="round"
-          initial={{ strokeDasharray: '24 10 6 0' }}
+          initial={{
+            strokeDasharray: [
+              '30 250 322 122',
+              '15 12 125 425',
+              '30 250 22 22',
+              '1000 0',
+            ],
+            scale: 1,
+            opacity: 0.8,
+          }}
           animate={{
-            strokeDasharray: ['15 120 25 25', '16 25 92 72', '4 250 22 22'],
-            rotate: [120, 360],
+            strokeDasharray: [
+              '30 250 322 122',
+              '15 12 125 425',
+              '30 250 22 22',
+              '1000 0',
+            ],
+            scale: [1, 1.2, 1],
+            opacity: [0.8, 1, 0.8],
+            rotate: [0, 360],
           }}
           transition={{
-            duration: 20,
+            duration: 8,
             repeat: Infinity,
-            repeatType: 'reverse',
+            repeatType: 'loop',
+            ease: 'easeInOut',
           }}
         />
       </motion.svg>
